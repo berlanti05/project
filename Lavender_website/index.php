f5ee624719046bf3f87662830a0c70119359fb51
berlanti <berlantemasalha@gmail.com>
2025-12-25 17:58:53 +0000

    Auto commit: Thu Dec 25 05:58:53 PM UTC 2025

--- Lavender_website/index.php
+++ Lavender_website/index.php
@@ -29,7 +29,7 @@
     </nav>
 -->
     <div class="banner-content">
-        <h1>LAVENDER SWEET</h1>
+        <h1>berlanti</h1>
         <p>"Indulge in the elegance of Lavender Sweet <br> where chocolate becomes an art of luxury."</p>
     </div>
 </section>
