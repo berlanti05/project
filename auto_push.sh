15aeee0152a815b56510513b9cf307c818c73cf4
berlanti <berlantemasalha@gmail.com>
2025-12-26 16:27:11 +0000

    Auto commit: Fri Dec 26 04:27:11 PM UTC 2025

--- auto_push.sh
+++ auto_push.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+if ! [ -d .git ]
+    then 
+         echo "Not a git repository"
+         exit 0
+fi
+
 git add .
+Branch=$(git branch | grep '*' | awk '{print $2}')
 message="Auto commit: $(date)"
 git commit -m "$message"
-git push origin main
-echo "Code pushed successfully"
+if [ $? -eq 0 ] 
+    then
+        git push origin $Branch
+        echo "Code pushed successfully"
+    else
+        echo "Nothing to commit"
+fi
